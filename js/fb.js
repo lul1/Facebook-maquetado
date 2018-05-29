ee3e0cb498fdf909249e4b5a472df7c1ab69c900
lul1 <pkevin2007@hotmail.com>
2018-05-28 21:20:55 -0500

    tercer commit

--- js/fb.js
+++ js/fb.js
@@ -1,6 +1,6 @@
 window.fbAsyncInit = function() {
     FB.init({
-        appId      : '812488395616576',
+        appId      : '162622677739483',
         cookie     : true,  // enable cookies to allow the server to access
             // the session
         xfbml      : true,  // parse social plugins on this page
