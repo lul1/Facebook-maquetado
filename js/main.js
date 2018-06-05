c3dc96d6073c5e96b3bd42e936a55358d2d1feb7
lul1 <pkevin2007@hotmail.com>
2018-06-05 13:41:53 -0500

    uLTIMO commit

--- js/main.js
+++ js/main.js
@@ -47,6 +47,8 @@ $(document).ready(function(){
   });
 });
 
+
+
 /* connect con facebook */
 
 
@@ -118,3 +120,70 @@ var Scene8Right = new ScrollMagic.Scene({
 })
 .setClassToggle('.containerform', 'show')
 .addTo(controller);
+
+
+/* validacion sign up */
+function validar() {
+
+  var username = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var contraseña = document.getElementById('contraseña').value;
+  var ccontraseña = document.getElementById('confirmar').value;
+  var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+  if (username == "") {
+    alert(" Escriba su nombre");
+    return false;
+  }
+
+  if (email == "") {
+    alert(" Escriba su correo");
+    return false;
+  }
+
+  if (contraseña == "") {
+    alert(" Escriba su contraseña");
+    return false;
+  }
+
+  if (ccontraseña == "") {
+    alert(" vuelva a escribir su contraseña");
+    return false;
+  }
+
+
+  if (contraseña !== ccontraseña) {
+    alert("La contraseña no coincide");
+    return false;
+  }
+
+  if (!regex.test(email)) {
+    alert("correo invalido");
+    return false;
+  }
+
+}
+function soloLetras(e){
+
+    key=e.keycode || e.which;
+
+    teclado=String.fromCharCode(key).toLowerCase();
+
+    letras="abcdefghijklmnñopqrstuvwxyz";
+
+    especiales="8-37-38-46-164";
+
+    teclado_especial=false;
+
+    for (var i in especiales) {
+      if (key==especiales[i]) {
+        teclado_especial=true;
+        break;
+      }
+    }
+
+    if (letras.indexOf(teclado)==-1  && !teclado_especial) {
+
+      return false;
+    }
+}
